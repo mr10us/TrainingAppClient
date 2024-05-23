6694725612e49281a348080973bd2d9c661fdfce
mr10us <minenkoduck@gmail.com>
2024-05-23 13:18:44 +0300

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
     <MainLayout>
       <div className="flex flex-col justify-between h-screen">
         <div className="pt-8 px-4 flex flex-row-reverse justify-between">
-          <pre>{user}</pre>
+          <p className="text-wrap">{JSON.stringify(user)}</p>
           {image ? (
             <motion.img
               className="size-16 object-contain"
