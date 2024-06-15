282b3df1fe022ed7ad41e80a1dce263b0312b744
mr10us <minenkoduck@gmail.com>
2024-06-15 16:19:46 +0300

    ...

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,3 +1,17 @@
 export const MainLayout = (props) => {
-  return <div {...props} style={{height: "fit-content"}}>{props.children}</div>;
+  const [height, setHeight] = useState("100svh");
+
+  window.Telegram.WebApp.onEvent("viewport_changed", function () {
+    const viewport = window.Telegram.WebApp.viewport;
+
+    setHeight(viewport.height + "px");
+  });
+
+  window.Telegram.WebApp.viewportChanged();
+
+  return (
+    <div {...props} style={{ height: height }}>
+      {props.children}
+    </div>
+  );
 };
